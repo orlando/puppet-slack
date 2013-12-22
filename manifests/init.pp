6ada973994969a1cae263d4b1f426b679e390a57
Orlando Del Aguila <orlando@delagui.la>
2013-12-22 13:04:13 -0430

    update to 0.35

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,8 +4,8 @@
 #
 #   include slack
 class slack {
-  package { 'Slack_0.34':
+  package { 'Slack_0.35':
     provider => 'compressed_app',
-    source   => 'https://dl0tgz6ee3upo.cloudfront.net/production/app/builds/000/747/368/original/7eeaf12b3a8c610df252bf02713c6e29/Slack_0.34.zip'
+    source   => 'https://dl0tgz6ee3upo.cloudfront.net/production/app/builds/000/790/107/original/fe15e3be1cec08cd320c02c986ad40bf/Slack_0.35.zip'
   }
 }
--- spec/classes/slack_spec.rb
+++ spec/classes/slack_spec.rb
@@ -2,8 +2,8 @@ require 'spec_helper'
 
 describe 'slack' do
   it do
-    should contain_package('Slack_0.34').with({
-      :source   => 'https://dl0tgz6ee3upo.cloudfront.net/production/app/builds/000/747/368/original/7eeaf12b3a8c610df252bf02713c6e29/Slack_0.34.zip',
+    should contain_package('Slack_0.35').with({
+      :source   => 'https://dl0tgz6ee3upo.cloudfront.net/production/app/builds/000/790/107/original/fe15e3be1cec08cd320c02c986ad40bf/Slack_0.35.zip',
       :provider => 'compressed_app'
     })
   end
