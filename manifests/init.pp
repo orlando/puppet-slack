3a646f99f5655580dffe0809420c9b889c53550f
Josh Nichols <technicalpickles@github.com>
2014-04-20 11:45:26 -0400

    Fix package name so boxen doesn't try to manage /Applications/Slack.app.app

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,7 +4,7 @@
 #
 #   include slack
 class slack {
-  package { 'Slack.app':
+  package { 'Slack':
     provider => 'compressed_app',
     source   => 'https://dl0tgz6ee3upo.cloudfront.net/production/app/builds/000/889/136/original/74ec94d57c53951b76879297718f5b97/Slack.app.zip'
   }
