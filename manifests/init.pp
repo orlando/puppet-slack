da3377089beaf0e7521ee93de62a42af1726be17
Orlando Del Aguila <orlando@delagui.la>
2014-06-11 08:34:54 -0500

    deprecating package

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,9 +3,4 @@
 # Examples
 #
 #   include slack
-class slack {
-  package { 'Slack':
-    provider => 'compressed_app',
-    source   => 'https://dl0tgz6ee3upo.cloudfront.net/production/app/builds/000/889/136/original/74ec94d57c53951b76879297718f5b97/Slack.app.zip'
-  }
-}
+warning('This package is deprecated, please install Slack directly from the App Store')
--- spec/classes/slack_spec.rb
+++ spec/classes/slack_spec.rb
@@ -2,9 +2,5 @@ require 'spec_helper'
 
 describe 'slack' do
   it do
-    should contain_package('Slack').with({
-      :source   => 'https://dl0tgz6ee3upo.cloudfront.net/production/app/builds/000/889/136/original/74ec94d57c53951b76879297718f5b97/Slack.app.zip',
-      :provider => 'compressed_app'
-    })
   end
 end
